0836b05d5bf8d58205847a0e917e1863eca4baa0
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2020-05-14 19:06:09 -0400

    Updated crab info to reflect MASS source

--- R/crabSpecies.R
+++ R/crabSpecies.R
@@ -5,7 +5,7 @@
 #' A sample of 200 rock crabs (Leptograpsus variegatus) found in the southern subtropical Pacific Ocean.
 #' These are small sea crabs that grow at most to about 50 millimetres of shell width.
 #' 
-#' The data contain the sex and species of each crab as determined by the researchers (see reference).
+#' The data contain the sex and species of each crab as determined by the researchers (see references).
 #' 
 #' 
 #' @format A data frame with 200 rows and 2 variables
@@ -21,7 +21,12 @@
 #'  
 #'  There are 50 of each combination of factor levels.
 #'  
-#'  The data match (row for row) the data on the physical measurements on the purple rock crab given in \code{lepto}.
+#'  Data are a subset of the \code{crabs} data set from the \code{MASS} package.  
+#'  Only the species and sex variables appear here and their row order here now match (row for row) the
+#'  order of the rows from the physical size measurements on the purple rock crab given in \code{lepto}.
+#'  
+#'  This separation now allows clustering methods to be explored on the data set \code{lepto} and
+#'  compared to the classification by colour (Species) and sex given here.
 #' 
 #' @docType data
 #' 
@@ -29,7 +34,7 @@
 #' 
 #' @seealso  \link{\code{lepto}}
 #' 
-#' @keywords crabs nature
+#' @keywords crabs nature MASS
 #' 
 #' @author 
 #' R.W. Oldford
@@ -40,6 +45,8 @@
 #'   N.A. Campbell and R.J. Mahon (1974). "A multivariate study of variation in two species of rock crab of the genus Leptograpsus."
 #'   Australian Journal of Zoology 22, 417-425. DOI:10.1071/ZO9740417
 #'   
+#'   Venables, W. N. and Ripley, B. D. (2002) Modern Applied Statistics with S. Fourth edition. Springer.  DOI:10.1007/978-0-387-21706-2
+#'   
 #' @source 
-#' \url{https://doi.org/10.1071/ZO9740417}  
+#' \url{https://doi.org/10.1071/ZO9740417}  \url{https://doi.org/10.1007/978-0-387-21706-2}
 NULL
--- R/lepto.R
+++ R/lepto.R
@@ -11,11 +11,11 @@
 #
 #' @format A data frame with 200 rows and 5 variables
 #' \describe{
-#'  \item{front}{The length in millimetres of the crab's frontal lobe.}
-#'  \item{rear}{The length in millimetres of the crab's rear.}
-#'  \item{length}{This is a measurement of the length of the carapace of the crab in millimetres.}
-#'  \item{width}{This is a measurement of the width of the carapace of the crab in millimetres.}
-#'  \item{depth}{This is a measurement of the depth of thecrab body in millimetres.}
+#'  \item{front}{The length in millimetres of the crab's carapace frontal lobe region just before the "frontal tubercles".}
+#'  \item{rear}{The length in millimetres of the crab's carapace rear.}
+#'  \item{length}{This is a measurement (in mm) of the length  of the crab's carapace from front to rear along its centre line.}
+#'  \item{width}{This is a measurement (in mm) of the width of the carapace of the crab at its widest point.}
+#'  \item{depth}{This is a measurement (in mm) of the depth of the crab body.}
 #' }
 #' 
 #' There are known to be four distinguishable subsets in this data set.
@@ -25,12 +25,19 @@
 #'  These are physical measurements on the purple rock crab (Leptograpsus variegatus), a large eyed
 #'  crab found in the southern subtropical Pacific ocean. These are small sea crabs that grow at most to
 #'  about 50 millimetres of shell width. 
+#'  
+#'  Data are a subset of the \code{crabs} data set from the \code{MASS} package.  
+#'  Only the five size measurements or the crabs are given here and their order has been randomized.
+#'  
+#'  A good data set for cluster analysis
 #' 
 #' @docType data
 #' 
 #' @name lepto
 #' 
-#' @keywords crabs nature
+#' @seealso crabsSpecies
+#' 
+#' @keywords crabs nature MASS
 #' 
 #' @author 
 #' R.W. Oldford
@@ -41,6 +48,9 @@
 #'   N.A. Campbell and R.J. Mahon (1974). "A multivariate study of variation in two species of rock crab of the genus Leptograpsus."
 #'   Australian Journal of Zoology 22, 417-425. DOI:10.1071/ZO9740417
 #'   
+#'   Venables, W. N. and Ripley, B. D. (2002) Modern Applied Statistics with S. 
+#'   Fourth edition. Springer.  DOI:10.1007/978-0-387-21706-2
+#'   
 #' @source 
-#' \url{https://doi.org/10.1071/ZO9740417}  
+#' \url{https://doi.org/10.1071/ZO9740417}  \url{https://doi.org/10.1007/978-0-387-21706-2}
 NULL
